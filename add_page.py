d74906e3b84df853f6f25b37dfd724736e2aee9e
Gavin Wahl <gwahl@fusionbox.com>
2012-07-29 21:39:06 -0600

    finalize all the widget/node/data/content data naming changes
    Node is structure
    Content is content

--- add_page.py
+++ add_page.py
@@ -4,15 +4,15 @@ from widgy.models import *
 page = ContentPage.objects.create(
         title='widgy page'
         )
-page.root_widget = TwoColumnLayout.add_root().node
+page.root_node = TwoColumnLayout.add_root().node
 page.save()
 
 for i in range(3):
-    page.root_widget.data.left_bucket.data.add_child(TextContent,
+    page.root_node.content.left_bucket.content.add_child(TextContent,
             content='yay %s' % i
             )
 
 for i in range(2):
-    page.root_widget.data.right_bucket.data.add_child(TextContent,
+    page.root_node.content.right_bucket.content.add_child(TextContent,
             content='yay right bucket %s' % i
             )
--- widgy/models.py
+++ widgy/models.py
@@ -10,86 +10,86 @@ from treebeard.mp_tree import MP_Node
 
 
 class ContentPage(Page):
-    root_widget = models.ForeignKey('WidgetNode', null=True)
+    root_node = models.ForeignKey('Node', null=True)
 
     def to_json(self):
         return {
                 'id': self.id,
                 'title': self.title,
-                'root_node': self.root_widget.to_json(),
+                'root_node': self.root_node.to_json(),
                 }
 
     def render(self):
         return render_to_string('widgy/content_page.html', {
             'title': self.title,
-            'root_node': self.root_widget,
+            'root_node': self.root_node,
             })
 
-class WidgetNode(MP_Node):
-    data_type = models.ForeignKey(ContentType)
-    data_id = models.PositiveIntegerField()
-    data = generic.GenericForeignKey('data_type', 'data_id')
+class Node(MP_Node):
+    content_type = models.ForeignKey(ContentType)
+    content_id = models.PositiveIntegerField()
+    content = generic.GenericForeignKey('content_type', 'content_id')
 
     def save(self, *args, **kwargs):
         new = not self.id
-        super(WidgetNode, self).save(*args, **kwargs)
+        super(Node, self).save(*args, **kwargs)
         if new:
-            self.data.post_create()
+            self.content.post_create()
 
     def to_json(self):
         children = [child.to_json() for child in self.get_children()]
         return {
                 'id': self.id,
-                'content': self.data.to_json(),
+                'content': self.content.to_json(),
                 'children': children,
                 }
 
     def render(self):
-        return self.data.render()
+        return self.content.render()
 
     @staticmethod
     def validate_parent_child(parent, child):
-        return parent.data.valid_parent_of(child) and child.data.valid_child_of(parent)
+        return parent.content.valid_parent_of(child) and child.content.valid_child_of(parent)
 
 
 
-class WidgetData(models.Model):
-    _widget = generic.GenericRelation(WidgetNode,
-                               content_type_field='data_type',
-                               object_id_field='data_id')
+class Content(models.Model):
+    _node = generic.GenericRelation(Node,
+                               content_type_field='content_type',
+                               object_id_field='content_id')
 
     @property
     def node(self):
-        return self._widget.all()[0]
+        return self._node.all()[0]
 
     class Meta:
         abstract = True
 
-    def valid_child_of(self, widget_class):
+    def valid_child_of(self, content_class):
         return True
 
-    def valid_parent_of(self, widget_class):
+    def valid_parent_of(self, content_class):
         return False
 
     def add_child(self, cls, **kwargs):
         obj = cls.objects.create(**kwargs)
         node = self.node.add_child(
-                data=obj
+                content=obj
                 )
         return obj
 
     @classmethod
     def add_root(cls, **kwargs):
         obj = cls.objects.create(**kwargs)
-        node = WidgetNode.add_root(
-                data=obj
+        node = Node.add_root(
+                content=obj
                 )
         return obj
 
     def post_create(self):
         """
         Hook for custom code which needs to be run after creation.  Since the
-        `WidgetNode` must be created after the widget, any tree based actions
+        `Node` must be created after the widget, any tree based actions
         have to happen after the save event has finished.
         """
         pass
@@ -102,10 +102,10 @@ class WidgetData(models.Model):
                 }
 
 
-class Bucket(WidgetData):
+class Bucket(Content):
     title = models.CharField(max_length=255)
 
-    def valid_parent_of(self, widget_class):
+    def valid_parent_of(self, content_class):
         return True
 
     def to_json(self):
@@ -119,7 +119,7 @@ class Bucket(WidgetData):
             })
 
 
-class TwoColumnLayout(WidgetData):
+class TwoColumnLayout(Content):
     """
     On creation, creates a left and right bucket.
     """
@@ -129,8 +129,8 @@ class TwoColumnLayout(WidgetData):
             'right': Bucket,
             }
 
-    def valid_child_of(self, widget_class):
-        return isinstance(widget_class, ContentPage)
+    def valid_child_of(self, content_class):
+        return isinstance(content_class, ContentPage)
 
     def post_create(self):
         for bucket_title, bucket_class in self.buckets.iteritems():
@@ -140,20 +140,20 @@ class TwoColumnLayout(WidgetData):
 
     @property
     def left_bucket(self):
-        return [i for i in self.node.get_children() if i.data.title=='left'][0]
+        return [i for i in self.node.get_children() if i.content.title=='left'][0]
 
     @property
     def right_bucket(self):
-        return [i for i in self.node.get_children() if i.data.title=='right'][0]
+        return [i for i in self.node.get_children() if i.content.title=='right'][0]
 
     def render(self):
         return render_to_string('widgy/two_column_layout.html', {
-            'left_bucket': self.left_bucket.data,
-            'right_bucket': self.right_bucket.data,
+            'left_bucket': self.left_bucket.content,
+            'right_bucket': self.right_bucket.content,
             })
 
 
-class TextContent(WidgetData):
+class TextContent(Content):
     content = models.TextField()
 
     def to_json(self):
