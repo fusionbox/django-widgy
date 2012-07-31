24307b79e568922001d6cd925a5eebc41bd25972
Gavin Wahl <gwahl@fusionbox.com>
2012-07-31 16:29:37 -0600

    nonfunctional (so far) UrlconfIncludePage.can_add

--- urlconf_include/models.py
+++ urlconf_include/models.py
@@ -6,3 +6,8 @@ from mezzanine.pages.models import Page
 
 class UrlconfIncludePage(Page):
     urlconf_name = models.CharField(max_length=255)
+
+    def can_add(self, request):
+        # This won't actually do anything until
+        # https://github.com/stephenmcd/mezzanine/issues/349 is fixed
+        return False
