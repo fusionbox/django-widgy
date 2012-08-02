500ed99c9f9e07afcf54f2c60470d842b6fbecc0
Rocky Meza <rocky@fusionbox.com>
2012-08-02 11:36:02 -0600

    use relative paths

--- widgy/static/widgy/js/main.js
+++ widgy/static/widgy/js/main.js
@@ -14,11 +14,11 @@ require.config({
     }
   },
   paths: {
-  'jquery': '/static/widgy/js/lib/jquery',
-  'underscore': '/static/widgy/js/lib/underscore',
-  'backbone': '/static/widgy/js/lib/backbone',
-  'mustache': '/static/widgy/js/lib/mustache',
-  'fusionbox': '/static/widgy/js/lib/fusionbox',
-  'text': '/static/widgy/js/require/text'
+  'jquery': './lib/jquery',
+  'underscore': './lib/underscore',
+  'backbone': './lib/backbone',
+  'mustache': './lib/mustache',
+  'fusionbox': './lib/fusionbox',
+  'text': 'require/text'
   }
 });
