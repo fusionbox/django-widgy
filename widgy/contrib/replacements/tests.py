e788d415d233ea5a57c5a2d21f6c40116c1126e7
Chris Proto <cproto@fusionbox.com>
2012-08-22 08:45:35 -0600

    test suite should be broken up more

--- widgy/contrib/replacements/tests.py
+++ widgy/contrib/replacements/tests.py
@@ -1,22 +1,33 @@
 from django.test import TestCase
 
+
 class CurlieRegexTests(TestCase):
 
-    def test_curlie_regex_matches_non_curlies(self):
+    def test_no_curlie_regex_empty(self):
         from .models import no_curlies_regex
         assert no_curlies_regex.match(u'') is not None
+
+    def test_no_curlie_regex_matches_non_curlies_single_char(self):
+        from .models import no_curlies_regex
         assert no_curlies_regex.match(u'a') is not None
+
+    def test_no_curlie_regex_matches_non_curlies_many_chars(self):
+        from .models import no_curlies_regex
         assert no_curlies_regex.match(u'This has no curlies') is not None
 
-
-    def test_curlie_regex_doesnt_match_curlies(self):
+    def test_no_curlie_regex_doesnt_match_single_curlies(self):
         from .models import no_curlies_regex
         assert no_curlies_regex.match(u'{') is None
         assert no_curlies_regex.match(u'}') is None
         assert no_curlies_regex.match(u'{a}') is None
         assert no_curlies_regex.match(u'{a}') is None
+
+    def test_no_curlie_regex_doesnt_match_just_curlies(self):
+        from .models import no_curlies_regex
         assert no_curlies_regex.match(u'}}') is None
         assert no_curlies_regex.match(u'{{') is None
-        assert no_curlies_regex.match(u'{{a}}') is None
+
+    def test_no_curlie_regex_doesnt_match_with_characters_curlies(self):
+        from .models import no_curlies_regex
         assert no_curlies_regex.match(u'{{a}}') is None
         assert no_curlies_regex.match(u'{{ This has curlies }}') is None
