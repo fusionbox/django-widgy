d214124e75a03dcd77705060adc927795489e1d4
Chris Proto <chroto24+github@gmail.com>
2012-10-27 17:15:53 -0600

    Set default db backend to sqlite3

--- demo/settings.py
+++ demo/settings.py
@@ -179,9 +179,9 @@ STATICFILES_FINDERS = (
 DATABASES = {
     "default": {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.",
+        "ENGINE": "django.db.backends.sqlite3",
         # DB name or path to database file if using sqlite3.
-        "NAME": "",
+        "NAME": "sqlite_database",
         # Not used with sqlite3.
         "USER": "",
         # Not used with sqlite3.
