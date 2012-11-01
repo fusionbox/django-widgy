601396b84455122645f4b4ff9e021eefe3919c4d
Chris Proto <cproto@fusionbox.com>
2012-11-01 15:30:32 -0600

    Template debug

--- demo/settings.py
+++ demo/settings.py
@@ -125,6 +125,7 @@ LANGUAGE_CODE = "en"
 # are displayed for error pages. Should always be set to ``False`` in
 # production. Best set to ``True`` in local_settings.py
 DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 # Whether a user's session cookie expires when the Web browser is closed.
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
