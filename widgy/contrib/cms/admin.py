acb235c2223aa1e5ca119a471a67556df860ae03
Chris Proto <cproto@fusionbox.com>
2012-11-07 17:09:42 -0700

    unregister callout content

--- widgy/contrib/cms/admin.py
+++ widgy/contrib/cms/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from mezzanine.pages.admin import PageAdmin
 
-from widgy.contrib.cms.models import ContentPage, Callout, CalloutContent
+from widgy.contrib.cms.models import ContentPage, Callout
 from widgy.forms import WidgyFormMixin
 
 
@@ -22,7 +22,6 @@ class WidgyPageAdmin(PageAdmin):
 
 admin.site.register(ContentPage, WidgyPageAdmin)
 admin.site.register(Callout)
-admin.site.register(CalloutContent)
 
 # Remove built in Django and Mezzanine models from the admin center
 from django.contrib.auth.models import Group
