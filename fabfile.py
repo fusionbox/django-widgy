c7efac33fa4e47edbd941f93ea2c928327717e6d
Chris Proto <cproto@fusionbox.com>
2012-11-08 10:06:43 -0700

    add dstage to fabfile

--- fabfile.py
+++ fabfile.py
@@ -8,4 +8,12 @@ env.project_name = 'django-widgy'
 env.short_name = 'widgy'
 env.tld = ''
 
+
+def stage_with_docs(pip=False, migrate=False, syncdb=False, branch=None):
+    stage(pip=pip, migrate=migrate, syncdb=syncdb, branch=branch)
+    with cd('/var/www/%s%s/doc' % (env.project_name, env.tld)):
+        with virtualenv(env.short_name):
+            run("make html")
+
 stage = roles('dev')(stage)
+dstage = roles('dev')(stage_with_docs)
