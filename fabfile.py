7e0ef4ba74bf2d6ea93f49d88c58378e7a1f9106
Chris Proto <cproto@fusionbox.com>
2012-11-08 10:08:45 -0700

    Make this one role, stage

--- fabfile.py
+++ fabfile.py
@@ -9,11 +9,11 @@ env.short_name = 'widgy'
 env.tld = ''
 
 
-def stage_with_docs(pip=False, migrate=False, syncdb=False, branch=None):
-    stage(pip=pip, migrate=migrate, syncdb=syncdb, branch=branch)
+_stage = stage
+def stage(pip=False, migrate=False, syncdb=False, branch=None):
+    _stage(pip=pip, migrate=migrate, syncdb=syncdb, branch=branch)
     with cd('/var/www/%s%s/doc' % (env.project_name, env.tld)):
         with virtualenv(env.short_name):
             run("make html")
 
 stage = roles('dev')(stage)
-dstage = roles('dev')(stage_with_docs)
