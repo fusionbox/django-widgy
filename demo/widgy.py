b2e70673200902eab880c9020cc08321d0f997d9
Rocky Meza <rocky@fusionbox.com>
2013-01-11 16:13:33 -0700

    demo widget TwoContentLayout

--- demo/widgy.py
+++ demo/widgy.py
@@ -2,4 +2,14 @@ from __future__ import absolute_import
 
 from widgy.site import WidgySite
 
-widgy_site = WidgySite()
+
+class DemoWidgySite(WidgySite):
+    def valid_parent_of(self, parent, child_class, child=None):
+        from widgy.contrib.page_builder.models import Accordion, Section
+        from demo.demo_widgets.models import TwoContentLayout
+
+        if isinstance(parent, Accordion) and isinstance(parent.get_root(), TwoContentLayout) and issubclass(child_class, Section) and len(parent.children) >= 2:
+            return False
+        return super(DemoWidgySite, self).valid_parent_of(parent, child_class, child)
+
+widgy_site = DemoWidgySite()
