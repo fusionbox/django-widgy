e78cca29ecafe3bf5e4d8de556197b8063182309
Gavin Wahl <gwahl@fusionbox.com>
2013-01-14 14:32:03 -0700

    tox

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup
+import subprocess
 import os
 
 __doc__ = """
@@ -20,17 +21,42 @@ install_requires = [
     'six',
 ]
 
-version = '0.0.1'
+version = (0, 0, 1, 'alpha')
+
+def get_version():
+    number = '.'.join(map(str, version[:3]))
+    stage = version[3]
+    if stage == 'final':
+        return number
+    elif stage == 'alpha':
+        process = subprocess.Popen('git rev-parse HEAD'.split(), stdout=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        return number + '-' + stdout.strip()[:8]
 
 setup(
     name='widgy',
-    version=version,
+    version=get_version(),
     description=__doc__,
     long_description=read('README'),
     packages=[
         'widgy',
+        'widgy.templatetags',
+        'widgy.db',
+        'widgy.migrations',
         'widgy.contrib',
+        'widgy.contrib.form_builder',
+        'widgy.contrib.widgy_mezzanine',
+        'widgy.contrib.list_content_widget',
+        'widgy.contrib.replacements',
         'widgy.contrib.cms',
+        'widgy.contrib.page_builder',
+        'widgy.contrib.form_builder.migrations',
+        'widgy.contrib.widgy_mezzanine.migrations',
+        'widgy.contrib.replacements.migrations',
+        'widgy.contrib.cms.migrations',
+        'widgy.contrib.page_builder.db',
+        'widgy.contrib.page_builder.migrations',
+        'widgy.contrib.page_builder.forms'
     ],
     install_requires=install_requires,
     dependency_links=[
@@ -38,4 +64,5 @@ setup(
         'http://github.com/Kronuz/pyScss/tarball/master#egg=pyscss-1.1.4',
     ],
     zip_safe=False,
+    include_package_data=True,
 )
