1b6b864f6a0b8342134ef6ebe3fb5489260f5d7e
Rocky Meza <rocky@fusionbox.com>
2013-01-19 16:56:41 -0700

    Added UnknownWidget
    When a widget class has been deleted, `prefetch_tree` is no longer able
    to find the classes for content types and ends up raising
    `AttributeError: 'NoneType' object has no attribute 'objects'`.
    @gavinwahl proposed returning a noop content type that is not draggable,
    editable, and renders to an empty string.

---
+++ widgy/migrations/0003_auto__add_unknownwidget.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'UnknownWidget'
+        db.create_table('widgy_unknownwidget', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('_nodes', self.gf('widgy.generic.ProxyGenericRelation')(object_id_field='content_id', to=orm['widgy.Node'])),
+        ))
+        db.send_create_signal('widgy', ['UnknownWidget'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'UnknownWidget'
+        db.delete_table('widgy_unknownwidget')
+
+
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'widgy.node': {
+            'Meta': {'object_name': 'Node'},
+            'content_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'depth': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'numchild': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'path': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'widgy.unknownwidget': {
+            'Meta': {'object_name': 'UnknownWidget'},
+            '_nodes': ('widgy.generic.ProxyGenericRelation', [], {'object_id_field': "'content_id'", 'to': "orm['widgy.Node']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['widgy']
--- widgy/models/base.py
+++ widgy/models/base.py
@@ -162,7 +162,11 @@ class Node(MP_Node):
         content_types = ContentType.objects.in_bulk(contents.iterkeys())
         for content_type_id, content_ids in contents.iteritems():
             ct = content_types[content_type_id]
-            instances = ct.model_class().objects.filter(pk__in=content_ids)
+            model_class = ct.model_class()
+            if model_class:
+                instances = ct.model_class().objects.filter(pk__in=content_ids)
+            else:
+                instances = [UnknownWidget(ct, id) for id in content_ids]
             contents[content_type_id] = dict([(i.id, i) for i in instances])
 
         # Loop through the nodes both assigning the content instance and the
@@ -572,3 +576,20 @@ class Content(models.Model):
         return {
             'edit_template': self.get_form_template(request),
         }
+
+
+class UnknownWidget(Content):
+    deletable = False
+    draggable = False
+    editable = False
+
+    class Meta:
+        app_label = 'widgy'
+
+    def __init__(self, content_type, id, *args, **kwargs):
+        super(UnknownWidget, self).__init__(*args, **kwargs)
+        self.content_type = content_type
+        self.id = id
+
+    def render(self, *args, **kwargs):
+        return ''
