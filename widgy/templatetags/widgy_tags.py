f6b7c672502374a5b69d08d1c4f66ebf17e47796
Gavin Wahl <gwahl@fusionbox.com>
2013-01-31 12:08:38 -0700

    rm unused `verbatim` template tag

--- widgy/templatetags/widgy_tags.py
+++ widgy/templatetags/widgy_tags.py
@@ -3,37 +3,6 @@ from django import template
 register = template.Library()
 
 
-class VerbatimNode(template.Node):
-    def __init__(self, content):
-        self.content = content
-
-    def render(self, context):
-        return self.content
-
-
-@register.tag
-def verbatim(parser, token):
-    """
-    Stops the template engine from rendering the contents of this block tag.
-
-    Usage::
-
-        {% verbatim %}
-            {% don't process this %}
-        {% endverbatim %}
-
-    You can also designate a specific closing tag block (allowing the
-    unrendered use of ``{% endverbatim %}``)::
-
-        {% verbatim myblock %}
-            ...
-        {% endverbatim myblock %}
-    """
-    nodelist = parser.parse(('endverbatim',))
-    parser.delete_first_token()
-    return VerbatimNode(nodelist.render(template.Context()))
-
-
 @register.simple_tag(takes_context=True)
 def render(context, node):
     from widgy.models import Content
