986fdb35b0a0dd391985c138a79112242c58d653
Gavin Wahl <gwahl@fusionbox.com>
2013-04-05 14:40:55 -0600

    remember to call super!

--- demo/widgy.py
+++ demo/widgy.py
@@ -12,5 +12,7 @@ class DemoWidgySite(WidgySite):
     def valid_parent_of(self, parent, child_class, obj=None):
         if isinstance(parent, I18NLayout):
             return True
+        else:
+            return super(DemoWidgySite, self).valid_parent_of(parent, child_class, obj)
 
 widgy_site = DemoWidgySite()
