795b96d473e42f5fef368cec323fcc742bc61267
Antoine Catton <acatton@fusionbox.com>
2013-05-07 16:45:53 -0600

    widgy.contrib.urlconf_include should always be configured

---
+++ demo/demo_url/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import RedirectView
+
+from . import views
+
+urlpatterns = patterns('',
+    (r'^$', RedirectView.as_view(url='hello/')),
+    (r'^hello/', views.hello),
+)
--- widgy/contrib/urlconf_include/models.py
+++ widgy/contrib/urlconf_include/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
 from mezzanine.pages.models import Page
 
 URLCONF_INCLUDE_CHOICES = getattr(settings, 'URLCONF_INCLUDE_CHOICES', None)
+# Application developers using widgy.contrib.urlconf_include should set
+# settings.URLCONF_INCLUDE_CHOICES.
+# If URLCONF_INCLUDE_CHOICES is None, this lets the user enter any urlconf_name:
+#   * if urlconf_name module doesn't exist, this breaks the whole site
+#   * if urlconf_name is a module executing code during the importation,
+# this can lead to remote code execution.
+if URLCONF_INCLUDE_CHOICES is None:
+    raise ImproperlyConfigured('URLCONF_INCLUDE_CHOICES setting should '
+                               'be a list of choices of urls modules')
 
 
 class UrlconfIncludePage(Page):
