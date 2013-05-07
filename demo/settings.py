5a9a9289ba896609cc61da1d23c63689aeb0cc17
Gavin Wahl <gwahl@fusionbox.com>
2013-05-06 19:44:19 -0600

    Install the template debug panel in the demo site

--- demo/settings.py
+++ demo/settings.py
@@ -413,7 +413,18 @@ DAISYDIFF_JAR_PATH = os.path.join(PROJECT_ROOT, '..', 'bin', 'daisydiff', 'daisy
 #     "ADMIN_PASS": "", # Live admin user password
 # }
 
-
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+    'widgy.debugtoolbar.templates.TemplatePanel',
+)
 ##################
 # LOCAL SETTINGS #
 ##################
