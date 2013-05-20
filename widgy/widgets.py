d63b0aa4f7525729f810d818d9923f71987693c6
Antoine Catton <acatton@fusionbox.com>
2013-05-20 14:06:19 -0600

    Fix datetime field in page_builder

---
+++ widgy/widgets.py
@@ -0,0 +1,18 @@
+from django.contrib.admin import widgets
+from django.template.loader import render_to_string
+
+
+class DateTimeMixin(object):
+    class Media:
+        extend = False
+        js = tuple()
+
+    def render(self, name, value, attrs=None):
+        widget = super(DateTimeMixin, self).render(name, value, attrs)
+        return render_to_string('page_builder/datetime_widget.html', {
+            'widget': widget,
+        })
+
+
+class DateTimeWidget(DateTimeMixin, widgets.AdminSplitDateTime):
+    pass
