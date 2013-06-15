e30b828f1785828d723273f82636b43ba3fdfeae
Gavin Wahl <gwahl@fusionbox.com>
2013-06-14 18:26:27 -0600

    login_required support for UrlconfIncludePages
    For anonymous users, we don't install the login_required urls. This
    means that when they visit a login_required url, they get a 404 -- not
    the expected redirect to the login page. To rectify this, try resolving
    the request again, this time only using UrlconfIncludePages. If it
    resolves this time, we know to redirect_to_login.

--- widgy/contrib/urlconf_include/middleware.py
+++ widgy/contrib/urlconf_include/middleware.py
@@ -5,6 +5,7 @@ from django.utils.importlib import import_module
 from django.conf import settings
 from django.conf.urls.defaults import include, url, patterns
 from django.conf.urls.i18n import i18n_patterns
+from django.core import urlresolvers
 
 from .models import UrlconfIncludePage
 
@@ -14,16 +15,19 @@ class PatchUrlconfMiddleware(object):
         root_urlconf = getattr(request, 'urlconf', settings.ROOT_URLCONF)
         if isinstance(root_urlconf, basestring):
             root_urlconf = import_module(root_urlconf)
-
-        request.urlconf = self.get_urlconf(root_urlconf)
+        request.urlconf = self.get_urlconf(root_urlconf, logged_in=request.user.is_authenticated())
 
     @classmethod
     def get_pattern_for_page(cls, page):
         return patterns('', url(r'^' + re.escape(page.slug) + '/', include(page.urlconf_name)))
 
     @classmethod
-    def get_urlconf(cls, root_urlconf):
-        urlconf_pages = list(UrlconfIncludePage.objects.all())
+    def get_urlconf(cls, root_urlconf, logged_in=True):
+        qs = UrlconfIncludePage.objects.published()
+        if not logged_in:
+            qs = qs.exclude(login_required=True)
+
+        urlconf_pages = list(qs)
         urlconf_pages.sort(key=lambda p: len(p.slug))
 
         new_urlconf = imp.new_module('urlconf')
@@ -38,6 +42,25 @@ class PatchUrlconfMiddleware(object):
             new_urlconf.handler500 = root_urlconf.handler500
 
         return new_urlconf
+
+    def process_response(self, request, response):
+        if response.status_code == 404 and not request.user.is_authenticated():
+            # This 404 response might be because we never installed the
+            # login_required urlpatterns. To be sure, try to resolve
+            # the request's URL using just UrlconfIncludePages. If it
+            # resolves, we know we need to redirect_to_login instead of
+            # 404ing.
+            empty_urlconf = imp.new_module('urlconf')
+            empty_urlconf.urlpatterns = patterns('')
+            urlconf = self.get_urlconf(empty_urlconf, logged_in=True)
+            try:
+                urlresolvers.resolve(request.get_full_path(), urlconf)
+            except urlresolvers.Resolver404:
+                pass
+            else:
+                from django.contrib.auth.views import redirect_to_login
+                return redirect_to_login(request.get_full_path())
+        return response
 
 
 class I18nPatchUrlconfMiddleware(PatchUrlconfMiddleware):
