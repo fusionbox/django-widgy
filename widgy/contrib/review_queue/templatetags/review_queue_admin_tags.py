f229850addde8d451c3bbd8e85cbab206e147c2b
Gavin Wahl <gwahl@fusionbox.com>
2013-06-21 13:58:16 -0600

    Move review_queue templates and fix grouping

--- widgy/contrib/review_queue/templatetags/review_queue_admin_tags.py
+++ widgy/contrib/review_queue/templatetags/review_queue_admin_tags.py
@@ -6,9 +6,13 @@ from django.contrib.admin.templatetags.admin_list import (
 
 register = Library()
 
-@register.inclusion_tag('admin/widgy/versioncommit/change_list_results.html')
+@register.inclusion_tag('admin/review_queue/reviewedversioncommit/change_list_results.html')
 def result_list(cl):
-    return result_list_orig(cl)
+    ctx = result_list_orig(cl)
+    # our template needs each result (a list of strings), along with the
+    # actual commit object.
+    ctx['cl_results'] = zip(ctx['cl'].result_list, ctx['results'])
+    return ctx
 
 # This avoid importing admin_list tag which could conflict with result_list
 # This does exactly the same as django.contrib.admin.templatetags.admin_list.admin_actions
