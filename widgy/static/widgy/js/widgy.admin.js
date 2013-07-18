79c15cf57af9fe0235171d1d5c2c9d12f288bb7a
Gavin Wahl <gwahl@fusionbox.com>
2013-07-18 16:54:46 -0600

    Move require.js's data-main configuration script inline
    require.js loads data-main asynchronously, so our inline javascript to
    instantiate widgy sometimes runs before require is configured. This
    results in require not being able to find libraries, and widgy fails to
    start. This fixes the intermittent 'blank widgy editor' bug.
    
    Also fixes a hardcoding of `/static/` paths when `{% static %}` should
    be used instead.

--- widgy/static/widgy/js/widgy.admin.js
+++ widgy/static/widgy/js/widgy.admin.js
@@ -11,3 +11,14 @@ jQuery(function($) {
   });
   window.closeFancybox = $.fancybox.close;
 });
+
+if ( typeof window.console == 'undefined' )
+{
+  var $console = $('<ul class="console" style="margin-bottom: 100px"></ul>');
+  $(document.body).append($console);
+  window.console = {
+    log: function(what) {
+      $console.append($('<li/>').text(what));
+    }
+  };
+}
