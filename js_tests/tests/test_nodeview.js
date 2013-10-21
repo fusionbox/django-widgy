f10de9a3dd7ed4f8c5957f066c7140fed5ae23d2
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-10-20 20:11:44 -0600

    Starting startDrag - issues with createDropTarget

--- js_tests/tests/test_nodeview.js
+++ js_tests/tests/test_nodeview.js
@@ -110,4 +110,40 @@ describe('CoreFunctions', function() {
     })
     .done();
   });
+
+  it('should startDrag', function() {
+    var nodes_promise = Q.all([this.node.ready(), this.node2.ready()]);
+    nodes_promise.then(function(node_array) {
+      var deferal = {};
+      _.extend(deferal, node_array[0]);
+      var myAPI = function() { return Q(deferal); };
+      sinon.stub(nodes.Node.prototype, 'getComponent', myAPI);
+      var app_view = new widgy.AppView({root_node: node_array[0], model: node_array[0]});
+      nodes.Node.prototype.getComponent.restore();
+
+      app_view.root_node_promise.then(function() {
+        node_array[1].component.View.prototype.title = 'Root Node';
+
+        var parent_view = app_view.node_view_list.at(0);
+        parent_view.$preview = parent_view.$(' > .widget > .preview ');
+        parent_view.$children = parent_view.$(' > .widget > .node_chidren ');
+
+        var draggable_view = new nodes.NodeView({model: node_array[1]});
+
+        var templateAPI = function() {
+          return Q('<span><%title%></span>').then(function() {
+            node_array[1].component.View.prototype.renderPromise.restore();
+            app_view.setCompatibility('1'); // This can probably go
+            app_view.node_view_list.at(1).$el.height(100);
+            app_view.node_view_list.at(0).startDrag(app_view.node_view_list.at(1));
+          });
+        }
+        sinon.stub(node_array[1].component.View.prototype, 'renderPromise', templateAPI);
+
+        deferal.children.add(node_array[1]);
+      })
+      .done();
+    })
+    .done();
+  });
 });
