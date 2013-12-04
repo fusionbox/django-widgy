6a9524502ebf3c04dede24fb937baec5c48342ef
Gavin Wahl <gwahl@fusionbox.com>
2013-12-04 14:16:26 -0700

    Use a more realistic context to render pages for search
    The Mezzanine page middleware adds a page and _current_page to the
    context for pages, so our search index should too.

--- widgy/contrib/widgy_mezzanine/search_indexes.py
+++ widgy/contrib/widgy_mezzanine/search_indexes.py
@@ -28,7 +28,8 @@ class PageIndex(indexes.SearchIndex, indexes.Indexable):
         return self.get_model().objects.published()
 
     def prepare_text(self, obj):
-        html = render_root({}, obj, 'root_node')
+        context = {'_current_page': obj.page_ptr, 'page': obj.page_ptr}
+        html = render_root(context, obj, 'root_node')
         content = html_to_plaintext(html)
         keywords = ' '.join(self.prepare_keywords(obj))
         return ' '.join([obj.title, keywords, obj.description,
