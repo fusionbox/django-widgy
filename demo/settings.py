feb68ad08ced23389a64ab880c4c527e607dbae5
Gavin Wahl <gwahl@fusionbox.com>
2014-02-14 10:57:26 -0700

    rm debug panels that debug toolbar removed

--- demo/settings.py
+++ demo/settings.py
@@ -93,13 +93,11 @@ SITE_ID = 1
 DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.version.VersionDebugPanel',
     'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
     'debug_toolbar.panels.headers.HeaderDebugPanel',
     'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
     'debug_toolbar.panels.template.TemplateDebugPanel',
     'debug_toolbar.panels.sql.SQLDebugPanel',
     'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
     'widgy.debugtoolbar.templates.TemplatePanel',
 )
 
