a8fa3b7885b8ce30f4d9eda5c01667edab8889f3
Rocky Meza <rocky@fusionbox.com>
2014-05-03 20:39:27 -0600

    Added tests for the preview view bugfixes.

--- tests/urls.py
+++ tests/urls.py
@@ -3,8 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+
+def dummy_view(*args, **kwargs):
+    pass
+
+
 urlpatterns = patterns('',
     url('^core_tests/', include('modeltests.core_tests.urls')),
     url("^admin/", include(admin.site.urls)),
     url('^widgy-mezzanine/', include('widgy.contrib.widgy_mezzanine.urls')),
+    # mezzanine.pages.views.page reverses the 'home' url.
+    url('^$', dummy_view, name='home'),
 )
