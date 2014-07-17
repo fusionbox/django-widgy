2585723d7c18b55bb5202446ab9c46f2cb178d62
Scott Clark <sclark@fusionbox.com>
2014-07-17 17:18:33 -0600

    Update demo project requirements
    Use Django 1.4 for LTS, Mezzanine 3.1.2 for pages context
    processors, required django-filer version from Widgy, and
    django-debug-toolbar 1.0.0 for new panel locations

--- demo/settings.py
+++ demo/settings.py
@@ -84,6 +84,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.media",
     "django.core.context_processors.request",
     "mezzanine.conf.context_processors.settings",
+    "mezzanine.pages.context_processors.page",
 )
 
 PACKAGE_NAME_FILEBROWSER = "filebrowser_safe"
@@ -94,15 +95,15 @@ ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
 SITE_ID = 1
 
 DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
     'widgy.debugtoolbar.templates.TemplatePanel',
 )
 
