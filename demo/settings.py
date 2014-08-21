20a7436fceefb7b900ab753163a4b868c0d39564
Antoine Catton <acatton@fusionbox.com>
2014-08-21 20:26:19 +0200

    Remove DEBUG_TOOLBAR_PANELS
    The new version of django-debug-toolbar doesn't have any module
    debug_toolbar.panels.settings_var.
    
    And 5a9a9289 don't give any reason why we should override the default
    django-debug-toolbar panels.

--- demo/settings.py
+++ demo/settings.py
@@ -93,19 +93,6 @@ GRAPPELLI_INSTALLED = True
 ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
 SITE_ID = 1
 
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-    'widgy.debugtoolbar.templates.TemplatePanel',
-)
-
 ROOT_URLCONF = 'demo.urls'
 
 WSGI_APPLICATION = 'demo.wsgi.application'
