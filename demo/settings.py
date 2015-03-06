348018ee2306e6e44918bd3e0491a9977918c043
Gavin Wahl <gwahl@fusionbox.com>
2015-03-06 10:27:42 -0700

    django 1.7 support for demo

--- demo/settings.py
+++ demo/settings.py
@@ -1,5 +1,6 @@
 import os
 import imp
+import django
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -50,13 +51,15 @@ INSTALLED_APPS = (
     'compressor',
     'scss',
     'sorl.thumbnail',
-    'south',
     'debug_toolbar',
     'django_extensions',
     'argonauts',
 
     'demo.demo_widgets',
 )
+
+if django.VERSION < (1, 7):
+    INSTALLED_APPS+= ('south',)
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
