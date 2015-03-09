d5bcff972e02f1a7d7437cb0f2ab76c71d7ff40c
Gavin Wahl <gwahl@fusionbox.com>
2015-03-09 16:18:07 -0600

    Don't allow EXTRA_INSTALLED_APPS to add duplicates
    This is necessary because the base test settings include review_queue.

--- tests/settings_contrib.py
+++ tests/settings_contrib.py
@@ -8,7 +8,9 @@ PACKAGE_NAME_GRAPPELLI = "grappelli_safe"
 GRAPPELLI_INSTALLED = True
 
 #INSTALLED_APPS += excavator.env_list('EXTRA_INSTALLED_APPS', required=True)
-INSTALLED_APPS += excavator.env_list('EXTRA_INSTALLED_APPS', required=True)
+INSTALLED_APPS += [
+    i for i in excavator.env_list('EXTRA_INSTALLED_APPS', required=True) if i not in INSTALLED_APPS
+]
 
 
 TEMPLATE_CONTEXT_PROCESSORS += (
