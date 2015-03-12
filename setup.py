906f043f57a9838e20d10f64e118dca17602fb5c
Gavin Wahl <gwahl@fusionbox.com>
2015-03-12 14:26:24 -0600

    set up for zest.releaser

--- setup.py
+++ setup.py
@@ -48,24 +48,10 @@ extras_require = {
 
 extras_require['all'] = set(j for i in extras_require.values() for j in i)
 
-STAGE = 'alpha'
-
-version = (0, 4, 0, STAGE)
-
-
-def get_version():
-    number = '.'.join(map(str, version[:3]))
-    stage = version[3]
-    if stage == 'final':
-        return number
-    elif stage == 'alpha':
-        process = subprocess.Popen('git rev-parse HEAD'.split(), stdout=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        return number + '-' + stdout.strip()[:8]
 
 setup(
     name='django-widgy',
-    version=get_version(),
+    version='0.4.0.dev0',
     author='Fusionbox, Inc.',
     author_email='programmers@fusionbox.com',
     description=__doc__,
