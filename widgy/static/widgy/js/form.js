7a70ebaaf4df67ef73dc1372e8aa134feb31adea
Julian Andrews <jandrews@fusionbox.com>
2017-02-24 13:24:42 -0700

    Fix related lookup JS bug
    We need to attach events handlers for popups when loading a page via
    ajax.

--- widgy/static/widgy/js/form.js
+++ widgy/static/widgy/js/form.js
@@ -26,7 +26,7 @@ define([
         if (!ret[name].push) {
           ret[name] = [ret[name]];
         }
-        ret[name].push($(this).getValue()); 
+        ret[name].push($(this).getValue());
       } else {
         ret[name] = $(this).getValue();
       }
@@ -39,6 +39,42 @@ define([
       'click input[type=submit], button[type=submit]': 'handleClick',
       // TODO: make sure this works with whatever
       'keydown :input:not(textarea):not(select)': 'handleKeypress'
+    },
+
+    renderHTML: function(template) {
+      // BBB: Django 1.9 and 1.10 attach event handlers on document ready, but
+      // we need to attach them after rendering the page. In Django 1.11 the handlers
+      // will attach to 'body' so this shouldn't be a problem anymore.
+      //
+      // See https://code.djangoproject.com/ticket/26357
+
+      var ret = Backbone.View.prototype.renderHTML.call(this, template);
+
+      // Only attach the handler if there isn't a similar handler attached to body
+      // XXX: This should prevent this code from running on Django 1.11, but
+      // this needs testing.
+      var clickSelectors = _.pluck(django.jQuery._data(document.body).events.click, 'selector');
+      if (!_.contains(clickSelectors, '.related-lookup')) {
+        this.$el.find('.related-lookup').click(function(e) {
+            e.preventDefault();
+            var event = $.Event('django:lookup-related');
+            $(this).trigger(event);
+            if (!event.isDefaultPrevented()) {
+                window.showRelatedObjectLookupPopup(this);
+            }
+        });
+      }
+      if (!_.contains(clickSelectors, '.add-another')) {
+        this.$el.find('.add-another').click(function(e) {
+            e.preventDefault();
+            var event = $.Event('django:add-another-related');
+            $(this).trigger(event);
+            if (!event.isDefaultPrevented()) {
+                window.showAddAnotherPopup(this);
+            }
+        });
+      }
+      return ret;
     },
 
     handleClick: function(event) {
